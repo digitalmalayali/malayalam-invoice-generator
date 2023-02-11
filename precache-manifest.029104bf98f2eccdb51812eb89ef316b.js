2b3ca1a24385f31130ab296008d2478cc859f90b
zcraber <61133303+zcraber@users.noreply.github.com>
2023-02-12 04:45:48 +0530

    Updates

---
+++ precache-manifest.029104bf98f2eccdb51812eb89ef316b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b936ef03bc26c9a94a563f9e83c02e0",
+    "url": "/malayalam-invoice-generator/index.html"
+  },
+  {
+    "revision": "263a86250af77c5f0823",
+    "url": "/malayalam-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "68d5c83656a0f24b9f81",
+    "url": "/malayalam-invoice-generator/static/css/main.373e9005.chunk.css"
+  },
+  {
+    "revision": "263a86250af77c5f0823",
+    "url": "/malayalam-invoice-generator/static/js/2.1edc723a.chunk.js"
+  },
+  {
+    "revision": "5fae47d0a016a5b68e2af829b08bec2a",
+    "url": "/malayalam-invoice-generator/static/js/2.1edc723a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d5c83656a0f24b9f81",
+    "url": "/malayalam-invoice-generator/static/js/main.ef307dc0.chunk.js"
+  },
+  {
+    "revision": "7e04152ae1998820b94f",
+    "url": "/malayalam-invoice-generator/static/js/runtime-main.0d9359dc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/malayalam-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "e631f0ca65031b20fe23668e166aa85b",
+    "url": "/malayalam-invoice-generator/static/media/remove.e631f0ca.svg"
+  },
+  {
+    "revision": "a700735566a3701072afbd164b80e690",
+    "url": "/malayalam-invoice-generator/static/media/resize.a7007355.svg"
+  }
+]);
